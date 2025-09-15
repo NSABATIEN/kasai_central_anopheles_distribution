f6390dafe63841f9fce88320e1b68c1c3861c40e
Victoire Nsabatien <vnsabatien@gmail.com>
2025-09-15 10:36:02 +0300

    go to a kc data path and get start reading kc data file

--- R/read_in_kc_data.R
+++ R/read_in_kc_data.R
@@ -14,21 +14,26 @@ library(naniar)
 
 # Make a path for David's computer
 dhd_data_path <- "~/pCloud Drive/R/data/va/vic/kc/raw"
+vic_data_path <- "V:/1. Vector Atlas and my PhD at LSTM/1. My PhD with LSTM/1. LSTM PhD work project/1.kc_entomo_database"
 
 # Make a path for Vic's computer
 
-get_data_folder_path <- function(user = c("dhd", "vic", "ger", "nick")) {
-  if (user == "dhd") {
-    path <- dhd_data_path
-  } else {
-    message("where is the data on your computer? Let's talk about this.")
-  }
-  return(path)
-}
+# get_data_folder_path <- function(user = c("dhd", "vic", "ger", "nick")) {
+#   if (user == "dhd") {
+#     path <- dhd_data_path
+#   } else {
+#     message("where is the data on your computer? Let's talk about this.")
+#   }
+#   return(path)
+# }
 
-data_folder_path <- get_data_folder_path("dhd")
+# data_folder_path <- get_data_folder_path("dhd")
+
+data_folder_path <- vic_data_path
 
 # Get all Excel files
+
+
 files <- list.files(
   data_folder_path,
   pattern = "^[0-9]{8}_drc_entomo_database_kc.xlsx?$",
@@ -37,6 +42,8 @@ files <- list.files(
 
 # Find the most recent by modification time
 latest_file <- files[which.max(file.info(files)$mtime)]
+# need to understand the concept of index (vic)
+# files[which.max(file.info(files)$size)] #(this ligne of code is not concern my own code but it's can help me to understand the concept about file info wich is can be the size, mtime, ctime or actime :keep it in mind) 
 
 # Is it a multisheet file?
 
@@ -44,20 +51,21 @@ excel_sheets(latest_file)
 
 # Sheet 1 is the collection data, as the name suggests
 read_excel(latest_file, sheet = 1) |> glimpse()
+# need to learn what glimpse doing  #(glimpse is the function)
 
 # Sheet 4 looks like it has the location info
 read_excel(latest_file, sheet = "location") |> glimpse()
 
 ## Step 3 : Now let read in collection data from MS Excel. # Vic had already fixed all the column names, but if not we could use a call to janitor::clean_names(kc_mosq). Let's show how anyway
 
-kc_mosq <- read_excel(latest_file, sheet = "collection_data") |>
+kc_mosq <- read_excel(latest_file, sheet = "data") |>
   clean_names()
 
 ## Visualize the data
 
 kc_mosq |> glimpse()
 
-# the date is not recognised as a date, let's fix that
+# the date is not recognised as a date, let's fix that # (need to keep it in your mind : POSIXct wich is mean date)
 
 kc_mosq <- kc_mosq |>
   mutate(
@@ -71,7 +79,20 @@ kc_mosq <- kc_mosq |>
 # We probably don't want repeat month data, Vic, so what do we want?  Also, we don't need all of these fields, which ones do we need
 
 # Step 4: Make a summary of the data that makes sense for the Anopheles distribution and composition model, and drop the fields that won't be useful for the modelling steps
+library(dplyr)
 
+kc_mosq |> 
+  dplyr::select(-collection_month,
+                -date,
+                -unique_initials_of_health_area_and_village,
+                -unique_collection,
+                -sample_transferred_pool_to_kasai_central,
+                -sample_transferred_kasai_central_to_kinshasa,
+                -comment) |> 
+  group_by(village,house_number) |> 
+  summarise(total_count = sum(n_anopheles_collected),.groups = "drop")
+
+ 
 message("Let's talk about this!")
 
 # Let's see how to join the location information
