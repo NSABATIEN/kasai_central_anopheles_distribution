a13ee99eeda00e0844d16ebe466a606712524cf2
Victoire Nsabatien Nsongtsa <vnsabatien@gmail.com>
2026-03-09 20:44:12 +0100

    crop to the extent around the study area and load the data coordinates. The gps coordinates need to be update (benatshiadi gps locations are outside the kc map)

--- R/prep_covariates.R
+++ R/prep_covariates.R
@@ -36,4 +36,50 @@ global_landcover <- c(global_trees,
                       global_mangroves,
                       global_moss)
 
+# crop to the extent around the study area : KC
+# get administrative area for a country
 
+drc_shp <-gadm(
+  country = "COD",
+  level= 0,
+  path= "data/downloads"
+)
+
+# Select KC
+
+kc<- drc_shp_prv |> 
+  filter(
+    NAME_1 == "Kasaï-Central"
+  )
+
+# extent around the study area
+kc_ext <- ext(kc)
+
+# crop rasters to KC extent
+bioclim_crop <- crop(drc_bioclim, kc_ext)
+landcover_crop <- crop(global_landcover, kc_ext)
+
+# plot
+plot (landcover_crop)
+
+writeRaster(
+  bioclim_crop,
+  "data/clean/bioclim_crop.tif"
+)
+
+writeRaster(
+  landcover_crop,
+  "data/clean/landcover_crop.tif"
+)
+
+
+# load the data coordinates, to transform covariates to maximise exaplanation of
+# variance across these locations
+coords <- read_csv("data/clean/kc_households_coords.csv",
+                   col_types = cols(
+                     health_zone = col_character(),
+                     health_area = col_character(),
+                     village = col_character(),
+                     lat_dd = col_double(),
+                     long_dd = col_double()
+                   )) 
